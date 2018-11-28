e41f7cf8dc265eea7932eccfa4b7eb7b2e4d7b7f
hanhan3682523 <250975141@qq.com>
2018-11-28 22:41:54 +0800

    【飘落的枫叶】脚手架完成，遍历templates，全部下载到本地，方便后续脚手架修改

--- index.js
+++ index.js
@@ -2,39 +2,12 @@
 
 //顶部"#! node"很重要，表示node来执行这个文件
 
-//创建用户输入的目录名称，如果没有输入则默认light-app
-let _userdir = 'light-app';
+let { downloadTemplate } = require('./readfile')
+    //创建用户输入的目录名称，如果没有输入则默认light-app
+let _userdir = '';
 if (process.argv.slice(2)[0]) {
     _userdir = process.argv.slice(2)[0];
 }
-
-let fs = require('fs');
-let path = require('path');
-
-let _src = path.join(__dirname, 'templates');
-fs.readdir(_src, function(err, files) {
-    if (err) {}
-    console.info('读取文件：', files);
-});
-
-function copyTemplate(from, to) {
-    from = path.join(__dirname, 'templates', from);
-    write(to, fs.readFileSync(from, 'utf-8'))
-}
-
-function write(path, str, mode) {
-    fs.writeFileSync(path, str)
-}
-
-function mkdir(path, fn) {
-    fs.mkdir(path, function(err) {
-        fn && fn()
-    });
-}
-
-let PATH = './' + _userdir;
-
-mkdir(PATH, function() {
-    //拷贝文件
-    copyTemplate('index.html', PATH + '/index.html')
-});
+console.log('脚手架开始创建.....');
+//下载模板
+downloadTemplate(_userdir);
